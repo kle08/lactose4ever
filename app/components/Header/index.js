660d924a9a4dcb220323ec0c20ecee26da8ddffe
iamasianman <kevinle1991@gmail.com>
2020-01-11 16:37:01 -0800

    added about me on the header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -21,6 +21,9 @@ function Header() {
         <HeaderLink to="/features">
           <FormattedMessage {...messages.features} />
         </HeaderLink>
+        <HeaderLink to="/about/kevin-le">
+          <FormattedMessage {...messages.about} />
+        </HeaderLink>
       </NavBar>
     </div>
   );
