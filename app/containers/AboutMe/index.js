22da6377b91d53e414f88443bd18de36d0ec13af
iamasianman <kevinle1991@gmail.com>
2020-01-15 15:47:14 -0800

    render server side data to the client side

--- app/containers/AboutMe/index.js
+++ app/containers/AboutMe/index.js
@@ -7,11 +7,39 @@ export default class AboutMe extends React.Component {
     this.state = {
       user: []
     }
+    this.getAboutMeData = this.getAboutMeData.bind(this)
   }
+
+  componentDidMount() {
+    this.getAboutMeData();
+  }
+
+  getAboutMeData() {
+    axios.get('http://localhost:3004/data')
+      .then((data) => {
+        this.setState({
+          user: data.data[0]
+        })
+      })
+      .catch((err) => {
+        console.log('err' + err)
+      })
+  }
+
   render() {
+    const imgStyle = {
+      borderRadius: '60%',
+      width: '20%',
+    };
+    const { name, img, blurb } = this.state.user;
     return (
       <>
-        <div>About Me: </div>
+        <h1>About Me: </h1>
+        <div>
+          <p>{name}</p>
+          <img src={img} style={imgStyle} />
+          <p>{blurb}</p>
+        </div>
       </>
     )
   }
