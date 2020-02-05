281bde47bcf47cf0a3d2252573d99aee02a20c7d
iamasianman <kevinle1991@gmail.com>
2020-02-04 21:55:53 -0800

    added constarnts.js

---
+++ app/containers/AboutMe/constants.js
@@ -0,0 +1,7 @@
+/*
+ * AboutMe constants
+ */
+
+export const BIO_REQUESTED = 'app/AboutMe/BIO_REQUESTED';
+export const BIO_SUCCEEDED = 'app/AboutMe/BIO_SUCCEEDED';
+export const BIO_FAILED = 'app/AboutMe/BIO_FAILED';
